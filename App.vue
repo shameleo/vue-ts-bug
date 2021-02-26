cdd4267d491832eb87788d03bf8c42e1afe807bb
Pavel Khmelevskiy <Khmelevskiy@irc.beltelecom.by>
2021-02-26 15:46:52 +0300

    init

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>whatever</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  data: () => ({
+    arr: [1, 2, 3].map((num) => ({ num: ref(num) })),
+  }),
+  mounted() {
+    this.arr.forEach((el) => {
+      console.log(el.num);
+      console.log(el.num.value);
+    });
+  },
+});
+</script>
+
+<style>
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = (env = {}/*, argv*/) => ({
+    context: __dirname,
+    mode: 'development',
+    devtool: false,                        
+    entry: {
+        'index': './index.js'    
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',       
+        filename: `[name].js`
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                    onlyCompileBundledFiles: true
+                }
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    stats: 'minimal'
+});
